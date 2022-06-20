451ef6e8039260c15cc4cf31475f83e6d70cb0a9
Gauthier Beaumont <gauthierbeaumont@gmail.com>
2022-06-20 23:01:24 +0200

    add localization

--- BoardGameLibrary/Program.cs
+++ BoardGameLibrary/Program.cs
@@ -1,7 +1,8 @@
-using GameBoardLibrary;
+using BoardGameLibrary;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using MudBlazor.Services;
+using Microsoft.Extensions.Localization;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
@@ -9,6 +10,8 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
+builder.Services.AddLocalization(options => options.ResourcesPath = "Resources");
+builder.Services.AddSingleton<IStringLocalizer<App>, StringLocalizer<App>>();
 builder.Services.AddMudServices();
 
 await builder.Build().RunAsync();
